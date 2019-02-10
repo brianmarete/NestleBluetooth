d37494cc936556a13b77016d1d19e10b8b3607f4
Brian Marete <bmarete10@gmail.com>
2019-02-10 16:05:28 +0300

    final .xml updates

--- app/src/main/java/com/moringaschool/nestlebluetooth/MainActivity.java
+++ app/src/main/java/com/moringaschool/nestlebluetooth/MainActivity.java
@@ -3,19 +3,45 @@ package com.moringaschool.nestlebluetooth;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.http.SslError;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_DISCOVERABLE_BT = 1;
+    public static final String TAG = MainActivity.class.getSimpleName();
     BluetoothAdapter mBluetoothAdapter;
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.wv_main);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                return false;
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        mWebView.loadUrl("https://nestle.m-vend.com/maintenances");
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
@@ -31,8 +57,6 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
 
-        WebView wv = findViewById(R.id.wv_main);
-        wv.loadUrl("file:///android_asset/checklist.html");
 
     }
 
@@ -41,8 +65,22 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Intent discoverableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableBtIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivityForResult(discoverableBtIntent, REQUEST_DISCOVERABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_DISCOVERABLE_BT:
+                if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, R.string.bluetooth_request_cancelled, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
         }
     }
 }
